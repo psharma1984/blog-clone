556639988ef341aa47f35b61d1ec9f52f7078cbc
psharma1984 <psharma.cad@gmail.com>
2023-05-28 00:16:38 -0700

    made final changes. all working

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from datetime import timezone
+from django.utils import timezone
 from typing import Any
 from django.urls import reverse_lazy
 from django.contrib.auth.decorators import login_required
@@ -20,7 +20,7 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=timezone.now()).orderby("-published_date")
+        return Post.objects.filter(published_date__lte=timezone.now()).order_by("-published_date")
 
 
 class PostDetailView(DetailView):
@@ -62,7 +62,7 @@ class DraftListView(LoginRequiredMixin, ListView):
 @login_required
 def post_publish(request, pk):
     post = get_object_or_404(Post, pk=pk)
-    post.publish
+    post.publish()
     return redirect("post_detail", pk=pk)
 
 
